b46f392c3cbc738f0c74f7f200bda151e4a64bcb
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-06-09 09:16:54 +0530

    Update pssd.js

---
+++ pssd.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+   strings: ["Student!","Web Developer!","Tech enthusiast!","Content Writer!"],
+   typeSpeed:100,
+   backSpeed:100,
+   backDelay:1000,
+   loop:true 
+});
