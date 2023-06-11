8b366fb62b52679893e280b3e78a3ad4030d9fc4
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-06-11 13:32:18 +0530

    Update pssd.js

--- pssd.js
+++ pssd.js
@@ -5,3 +5,28 @@ var typed= new Typed(".text",{
    backDelay:1000,
    loop:true 
 });
+var items = document.querySelectorAll("li");
+
+function isItemInView(item){
+  var rect = item.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isItemInView(items[i])) {
+        items[i].classList.add("show");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
