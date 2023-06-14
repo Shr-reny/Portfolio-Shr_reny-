a2f6428b2f1e5c3dabb8ca7f11c48f5c067fe1a6
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-06-14 06:28:59 +0530

    Update pssd.js

--- pssd.js
+++ pssd.js
@@ -25,7 +25,6 @@ function callbackFunc() {
     }
   }
 
-  // listen for events
   window.addEventListener("load", callbackFunc);
   window.addEventListener("resize", callbackFunc);
   window.addEventListener("scroll", callbackFunc);
